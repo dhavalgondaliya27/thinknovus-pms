85c663aab57c049f1c090c5a85dc14cbf73e3ec5
bhavinkarena <bhavinkarena2003@gmail.com>
2024-11-11 15:05:46 +0530

    setup response and errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
 const { app } = require('./app.js');
+const appConfig = require('./config/appConfig');
 
 dotenv.config({
   path: './.env',
@@ -8,8 +9,8 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8081, () => {
-      console.log(`🚀 Server is running at port no : 8081`);
+    app.listen(appConfig.port || 8081, () => {
+      console.log(`🚀 Server is running at port no : ${appConfig.port}`);
     });
   })
   .catch(err => {
