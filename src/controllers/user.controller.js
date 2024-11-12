0255da5d494a6cf7b2db97ad12ee3bc46c6e2c63
bhavinkarena <bhavinkarena2003@gmail.com>
2024-11-12 18:35:15 +0530

    Setup project and solve bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const userService = require('../services/user.service');
-// const ApiError = require('../utils/ApiError');
 const ApiError = require('../utils/ApiError');
 const ApiResponse = require('../utils/ApiResponse');
 const asyncHandler = require('../utils/asyncHandler');
@@ -14,11 +13,8 @@ exports.createUser = asyncHandler(async (req, res, next) => {
     if (error) {
       return next(new ApiError(error.message, STATUS_CODES.BAD_REQUEST));
     }
-    const userExists = await userService.findUserByEmail(email);
+    const userExists = await userService.findUserBEmail(email);
     if (userExists) {
-      console.log('wdwef');
-      // return next(new ApiError(STATUS_CODES.CONFLICT, null, 'user alreddy exist'));
-      // throw new ApiError(STATUS_CODES.CONFLICT, null, 'user alreddy exist');
       return next(new ApiError('user alreddy exist', STATUS_CODES.CONFLICT));
     }
     const user = await userService.createUser({ email, firstname, lastname });
@@ -26,10 +22,8 @@ exports.createUser = asyncHandler(async (req, res, next) => {
       .status(201)
       .json(new ApiResponse(201, user, 'User created successfully'));
   } catch (error) {
-    console.log(error);
-    // throw new ApiError(500, null, 'something want wrong');
     return next(
-      new ApiError('something want wrong', STATUS_CODES.SERVER_ERROR),
+      new ApiError(error.message, STATUS_CODES.SERVER_ERROR),
     );
   }
 });
--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -4,7 +4,7 @@ const userSchema = Joi.object({
   firstname: Joi.string().min(3).max(30).required(),
   lastname: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
+  // password: Joi.string().min(8).required(),
 });
 
 module.exports = userSchema;
