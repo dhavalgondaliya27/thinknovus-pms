bb5ec4901a52eea778c4ad197f9236d879bcc329
dhavalgondaliya27 <dhavalgondaliya2727@gmail.com>
2024-11-12 16:49:28 +0530

    Change APPError to APIError

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const userService = require('../services/user.service');
 // const ApiError = require('../utils/ApiError');
-const AppError = require('../utils/ApiError');
+const ApiError = require('../utils/ApiError');
 const ApiResponse = require('../utils/ApiResponse');
 const asyncHandler = require('../utils/asyncHandler');
 const { STATUS_CODES } = require('../utils/constants');
@@ -14,15 +14,15 @@ exports.createUser = asyncHandler(async (req, res, next) => {
       console.log('wdwef');
       // return next(new ApiError(STATUS_CODES.CONFLICT, null, 'user alreddy exist'));
       // throw new ApiError(STATUS_CODES.CONFLICT, null, 'user alreddy exist');
-      return next(new AppError('user alreddy exist', STATUS_CODES.CONFLICT));
+      return next(new ApiError('user alreddy exist', STATUS_CODES.CONFLICT));
     }
     const user = await userService.createUser({ email, firstname, lastname });
-    res
+    return res
       .status(201)
       .json(new ApiResponse(201, user, 'User created successfully'));
   } catch (error) {
     console.log(error);
     // throw new ApiError(500, null, 'something want wrong');
-    return next(new AppError('something want wrong', STATUS_CODES.SERVER_ERROR));
+    return next(new ApiError('something want wrong', STATUS_CODES.SERVER_ERROR));
   }
 });
