a84927799a49d291d2cf171e2357270629088455
bhavinkarena <bhavinkarena2003@gmail.com>
2024-11-21 18:31:23 +0530

    Create api for emp

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(
     origin: ['http://localhost:3000'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }),
 );
