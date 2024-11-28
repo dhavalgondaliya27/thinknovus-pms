fe1284388676a78d69571fa617ec8c221eb6f7ca
dhavalgondaliya27 <bhavnagondaliya77@gmail.com>
2024-11-28 09:50:08 +0530

    Done

--- src/routes/employee/employee.route.js
+++ src/routes/employee/employee.route.js
@@ -4,7 +4,11 @@ const { verifyJWT } = require('../../middleware/auth.middleware');
 
 const router = Router();
 
-router.post('/emp/create-emp', verifyJWT,empController.createEmployee);
-router.post('/emp/create-or-update-personal-info',verifyJWT, empController.createOrupdateEmployeePersonalInfo);
+router.post('/emp/create-emp', verifyJWT, empController.createEmployee);
+router.post(
+  '/emp/create-or-update-personal-info',
+  verifyJWT,
+  empController.createOrupdateEmployeePersonalInfo,
+);
 
 module.exports = router;
